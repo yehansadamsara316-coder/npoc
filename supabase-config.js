f877fefdd2863181c6c4dce3ed67d1615a419314
yehansadamsara316-coder <yehansadamsara316@gmail.com>
2026-09-12 21:27:39 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://azzwynmjuuuxwyflujjo.supabase.co/rest/v1/canteen_orders';
+const SUPABASE_ANON_KEY = 'sb_publishable_KNa1UDWQTQ9qtkcGGDrIdQ_hLPNVXoe';
+
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
